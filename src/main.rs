e78e80634e8ff40533800e16c655e34e14076102
winter.loo <adjkldd@126.com>
2024-11-20 03:58:21 +0800

    output echo response message as a json
    echo protocol: https://fly.io/dist-sys/1

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use serde_json::Value;
 use std::{io::{self, BufRead}, collections::HashMap};
 
 /// protocol specification from https://github.com/jepsen-io/maelstrom/blob/main/doc/protocol.md
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Message {
     src: String,
     dest: String,
     body: MessageBody,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct MessageBody {
     // reference: https://serde.rs/field-attrs.html
     #[serde(rename = "type")]
@@ -27,10 +27,24 @@ fn main() {
     for line in stdin.lines() {
         match line {
             Ok(content) => match serde_json::from_str::<Message>(&content) {
-                Ok(msg) => println!("{:#?}", msg),
+                Ok(msg) => println!("{}", serde_json::to_string(&handle_message(msg)).unwrap()),
                 Err(err) => eprintln!("invalid json data for message: {}", err),
             },
             Err(err) => eprintln!("Error reading line: {}", err),
         }
     }
+}
+
+fn handle_message(msg: Message) -> Message {
+    if msg.body.type_ == "echo" {
+        let mut res = msg.clone();
+        let req_id = msg.body.msg_id.unwrap();
+        res.src = msg.dest;
+        res.dest = msg.src;
+        res.body.type_ = "echo_ok".to_string();
+        res.body.msg_id = Some(req_id + 1);
+        res.body.in_reply_to = Some(req_id);
+        return res;
+    }
+    panic!("unknown message");
 }
